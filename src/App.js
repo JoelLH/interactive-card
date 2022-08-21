6948f80d5eb9bf9f4d468909b47b3faa4faa123d
JoelLH <jhectorlopezh@gmail.com>
2022-08-20 18:01:25 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import Cards from './components/Cards';
+
 
 function App() {
+  const[card, setCard] = useState({
+    name:"Jane Appleseed", 
+    number: "",
+    month: "00",
+    year: "00",
+    cvc:"123",
+    pop: false
+  })
+
+  function setPop(event){
+    const{name, number, month, year, cvc} = card
+    if(name != "" && number != ""  && month != "" && year != "" && cvc != ""){
+        setCard(prev=>({
+        ...prev,
+        pop : !prev.pop
+        }))
+    }
+    console.log(card.pop)
+    event.preventDefault()
+  }
+  function maxLength(event){
+    const {name, value} = event.target
+    switch(name){
+      case "name":
+        setCard(prev=>({
+        ...prev,
+        [name] : value
+        }))
+        break;
+      case "number":
+        setCard(prev=>({
+        ...prev,
+        [name] : value.slice(0, 16)
+        }))
+        break;
+      case "month":
+        setCard(prev=>({
+        ...prev,
+        [name] : value.slice(0, 2)
+        }))
+        break;
+      case "year":
+        setCard(prev=>({
+        ...prev,
+        [name] : value.slice(0, 2)
+        }))
+        break
+      case "cvc":
+        setCard(prev=>({
+        ...prev,
+        [name] : value.slice(0, 3)
+        }))
+        break;
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards card={card}/>
+      <Form 
+      // handleChange={handleChange}
+      card={card}
+      togglePop={setPop}
+      handleLength={maxLength}
+      />
     </div>
   );
 }
-
 export default App;
